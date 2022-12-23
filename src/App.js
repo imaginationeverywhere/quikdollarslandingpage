481e7dc066ba5001b39f3adefd685cfceaff3a14
Amen Moja Ra <mojaray2k@gmail.com>
2022-12-23 13:35:28 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import { TemplateRootRoot1 } from "./TemplateRootRoot1";
+
+export default function App() {
+  document.body.style.backgroundColor = "#f2f2f2";
+  document.body.style.margin = "0";
+  return <TemplateRootRoot1 />;
+}
---
+++ src/TemplateRootRoot1.js
@@ -0,0 +1,726 @@
+import React from "react";
+import styled from "styled-components";
+
+export const TemplateRootRoot1 = ({}) => {
+  const Button2Function = (e, name) => {
+    alert(`${name} was clicked`);
+  };
+  const Button4Function = (e, name) => {
+    alert(`${name} was clicked`);
+  };
+  const Button8Function = (e, name) => {
+    alert(`${name} was clicked`);
+  };
+  return (
+    <TemplateRootRootRoot>
+      <NewGroup>
+        <Bg5>
+          <FlexColumn1>
+            <Bg7>
+              <Shape8
+                src={`https://file.rendit.io/n/N3cnmCSX7wSlPFLTy7IR.svg`}
+              />
+            </Bg7>
+            <FlexColumn2>
+              <FlexColumn3>
+                <AmazingMobileAppL4>
+                  Discover
+                  <AmazingMobileAppL>
+                    {" "}
+                    <br />
+                  </AmazingMobileAppL>
+                  <AmazingMobileAppL1>Quik</AmazingMobileAppL1>
+                  <AmazingMobileAppL> </AmazingMobileAppL>
+                  <AmazingMobileAppL3>Dollars</AmazingMobileAppL3>
+                </AmazingMobileAppL4>
+                <CreativeMobileApp3>
+                  The Pocket Currency Exchange
+                </CreativeMobileApp3>
+              </FlexColumn3>
+              <Button2 onClick={(e) => Button2Function(e, "Button2")}>
+                <Button1>View the App</Button1>
+              </Button2>
+            </FlexColumn2>
+          </FlexColumn1>
+          <Text6>Features</Text6>
+          <FlexColumn4>
+            <FlexRow>
+              <FlexRow1>
+                <Text7>Showcase</Text7>
+                <Text7>Download</Text7>
+              </FlexRow1>
+              <Button4 onClick={(e) => Button4Function(e, "Button4")}>
+                <Button1>View the App</Button1>
+              </Button4>
+            </FlexRow>
+            <FlexColumn5>
+              <Oval1
+                src={`https://file.rendit.io/n/UNFH7Y9lQfIsYDAu3e9J.svg`}
+              />
+              <Qd1 src={`https://file.rendit.io/n/28IwVwnjjSgLU2DhEknQ.png`} />
+            </FlexColumn5>
+          </FlexColumn4>
+        </Bg5>
+        <Bg8>
+          <GetInTouchBy>Available on</GetInTouchBy>
+          <Googleplay
+            src={`https://file.rendit.io/n/QOhmBdQQtVcb3WVlvYCA.png`}
+          />
+          <Applelogo
+            src={`https://file.rendit.io/n/VVx2WRA12rDQCxxYThoq.png`}
+          />
+        </Bg8>
+      </NewGroup>
+      <NewGroup1>
+        <EasyToEdit>Easy to use</EasyToEdit>
+        <Text9>Endless Options</Text9>
+        <UXUIKit>Lowest Fees</UXUIKit>
+      </NewGroup1>
+      <NewGroup2>
+        <CeruleanFlexColumn>
+          <Shape4>
+            <Shape5 src={`https://file.rendit.io/n/Gq9eKAqNZutbShgjoAip.svg`} />
+          </Shape4>
+        </CeruleanFlexColumn>
+        <Lines>
+          <MacaroniAndCheeseFlexColumn>
+            <Shape6 src={`https://file.rendit.io/n/GAwxdBkchEfWm0d71dWC.svg`} />
+          </MacaroniAndCheeseFlexColumn>
+        </Lines>
+        <IndigoFlexColumn>
+          <Shape7 src={`https://file.rendit.io/n/E99lJ3qr8KwVU3jaard8.svg`} />
+        </IndigoFlexColumn>
+      </NewGroup2>
+      <NewGroup3>
+        {[
+          {
+            childText: `Description`,
+          },
+          {
+            childText: `Description`,
+          },
+          {
+            childText: `Description`,
+          },
+        ].map((data) => (
+          <CreativeMobileApp>{data.childText}</CreativeMobileApp>
+        ))}
+      </NewGroup3>
+      <NewGroup4>
+        <Bg3>
+          <Oval src={`https://file.rendit.io/n/LRgwAg4PxdN4dKavuV84.svg`} />
+        </Bg3>
+        <Group16>
+          <Group17>
+            <TrackShippments>View the entire ASD App Suite</TrackShippments>
+            <LoremIpsumDolorSi>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam.
+            </LoremIpsumDolorSi>
+            <Button8 onClick={(e) => Button8Function(e, "Button8")}>
+              <Button1>View the App</Button1>
+            </Button8>
+          </Group17>
+          <Group18>
+            <Oval1 src={`https://file.rendit.io/n/BtpMy8ujVeviA4z7M4me.svg`} />
+            <Black src={`https://file.rendit.io/n/oGyQV3KdDUxraYjzW36Y.png`} />
+          </Group18>
+        </Group16>
+        <Qd>
+          <TrackShippments>Download Quik Dollars Today!</TrackShippments>
+          <LoremIpsumDolorSi1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </LoremIpsumDolorSi1>
+          <Button8 onClick={(e) => Button8Function(e, "Button6")}>
+            <Button1>View the App</Button1>
+          </Button8>
+        </Qd>
+      </NewGroup4>
+      <NewGroup5>
+        <Bg />
+        <Bg1>
+          <Shape>
+            <Shape1 src={`https://file.rendit.io/n/DiMKGImNOgslNBt9UJbk.svg`} />
+          </Shape>
+          <Logofacebook
+            src={`https://file.rendit.io/n/sUIPeCdQEqRZ8SkDWbRT.svg`}
+          />
+          <Shape2 src={`https://file.rendit.io/n/ga5i8YWELANFmDrgaYiW.svg`} />
+          <Shape3 src={`https://file.rendit.io/n/jmC3YHLcviyXBE2Lez1K.svg`} />
+        </Bg1>
+      </NewGroup5>
+    </TemplateRootRootRoot>
+  );
+};
+
+const AmazingMobileAppL = styled.div`
+  display: contents;
+  color: #060c3a;
+  font-size: 60px;
+  font-family: Avenir;
+  line-height: 78px;
+  white-space: pre-wrap;
+  letter-spacing: -1px;
+`;
+const Button1 = styled.div`
+  color: #ffffff;
+  font-size: 18px;
+  font-weight: 500;
+  font-family: Avenir;
+  line-height: 28px;
+  white-space: nowrap;
+`;
+const Text7 = styled.div`
+  color: #ffffff;
+  font-size: 16px;
+  font-family: Avenir;
+  white-space: nowrap;
+`;
+const Oval1 = styled.img`
+  width: 43.75em;
+  height: 700px;
+  left: 0px;
+  top: 0px;
+  position: absolute;
+`;
+const CreativeMobileApp = styled.div`
+  width: 19.38em;
+  color: #8c8e9e;
+  font-size: 16px;
+  font-family: Roboto;
+  line-height: 28px;
+  text-align: center;
+`;
+const TrackShippments = styled.div`
+  margin: 0px 0px 1.42em 0em;
+  color: #060c3a;
+  font-size: 24px;
+  font-family: Roboto;
+  white-space: nowrap;
+  letter-spacing: -0.4px;
+`;
+const Button8 = styled.button`
+  height: 56px;
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0px 0px 0px 0em;
+  padding: 0px;
+  padding-right: 2.47em;
+  padding-left: 2.47em;
+  border-width: 0px;
+  border-radius: 6px;
+  box-sizing: content-box;
+  background-color: #16bb00;
+  cursor: pointer;
+  &: hover {
+    box-shadow: inset 0 0 100px 100px rgba(255, 255, 255, 0.3);
+  } ;
+`;
+const TemplateRootRootRoot = styled.div`
+  height: 4657px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 0em;
+  background-color: #ffffff;
+  overflow: hidden;
+`;
+const NewGroup = styled.div`
+  width: 73.13em;
+  height: 1150px;
+  position: relative;
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  margin: 0px 0px 17.5em 0px;
+  padding: 0em 8.44em;
+`;
+const Bg5 = styled.div`
+  width: 90em;
+  left: 0px;
+  top: 0px;
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 3.75em 0em 12.19em 0em;
+  background-image: linear-gradient(45deg, #174411 0%, #c8c8c8 100%);
+`;
+const FlexColumn1 = styled.div`
+  height: 484px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0px 1.38em 0px 0px;
+  padding: 0em 0em 19.44em 0em;
+`;
+const Bg7 = styled.div`
+  height: 50px;
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-self: flex-start;
+  align-items: center;
+  margin: 0px 0px 0px 0.56em;
+  padding: 0px 0.94em;
+  border-radius: 35px;
+  background-color: #ffffff;
+`;
+const Shape8 = styled.img`
+  width: 1.25em;
+  height: 12px;
+`;
+const FlexColumn2 = styled.div`
+  gap: 27px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 0em;
+`;
+const FlexColumn3 = styled.div`
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 0em 0px 0.56em;
+`;
+const AmazingMobileAppL4 = styled.div`
+  align-self: flex-start;
+  margin: 0px 0px 0px 0em;
+  font-size: 60px;
+  font-family: Avenir;
+  line-height: 78px;
+  white-space: nowrap;
+  letter-spacing: -1px;
+`;
+const AmazingMobileAppL1 = styled.div`
+  display: contents;
+  color: #16bb00;
+  font-size: 60px;
+  font-family: Avenir;
+  line-height: 78px;
+  letter-spacing: -1px;
+`;
+const AmazingMobileAppL3 = styled.div`
+  display: contents;
+  font-size: 60px;
+  font-family: Avenir;
+  line-height: 78px;
+  letter-spacing: -1px;
+`;
+const CreativeMobileApp3 = styled.div`
+  color: #ffffff;
+  font-size: 30px;
+  font-weight: 500;
+  font-family: Avenir;
+  line-height: 74.5999984741211px;
+  white-space: nowrap;
+  letter-spacing: 0.15px;
+`;
+const Button2 = styled.button`
+  height: 56px;
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-self: flex-start;
+  align-items: center;
+  margin: 0px 0px 0px 0em;
+  padding: 0px;
+  padding-right: 2.47em;
+  padding-left: 2.47em;
+  border-width: 0px;
+  border-radius: 6px;
+  box-sizing: content-box;
+  background-color: #16bb00;
+  cursor: pointer;
+  &: hover {
+    box-shadow: inset 0 0 100px 100px rgba(255, 255, 255, 0.3);
+  } ;
+`;
+const Text6 = styled.div`
+  align-self: flex-start;
+  margin: 0.94em 1.13em 0px 0px;
+  color: #060c3a;
+  font-size: 16px;
+  font-family: Avenir;
+  white-space: nowrap;
+`;
+const FlexColumn4 = styled.div`
+  gap: 39px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 0em;
+`;
+const FlexRow = styled.div`
+  width: 42.88em;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0em 4.69em 0em 0.88em;
+`;
+const FlexRow1 = styled.div`
+  gap: 34px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0.94em 0px 1.19em 0px;
+`;
+const Button4 = styled.button`
+  height: 56px;
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  padding-right: 2.47em;
+  padding-left: 2.47em;
+  border-width: 0px;
+  border-radius: 6px;
+  box-sizing: content-box;
+  background-color: #16bb00;
+  cursor: pointer;
+  &: hover {
+    box-shadow: inset 0 0 100px 100px rgba(255, 255, 255, 0.3);
+  } ;
+`;
+const FlexColumn5 = styled.div`
+  width: 43.13em;
+  height: 644px;
+  position: relative;
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 1.75em 0em 1.75em 5.31em;
+`;
+const Qd1 = styled.img`
+  width: 43.13em;
+  height: 622px;
+  position: relative;
+`;
+const Bg8 = styled.div`
+  width: 48.63em;
+  height: 114px;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-end;
+  padding: 2.31em 14.13em 3.06em 10.38em;
+  border-radius: 30px;
+  background-color: #ffffff;
+`;
+const GetInTouchBy = styled.div`
+  margin: 0px 5.23em 0.8em 0px;
+  color: #060c3a;
+  font-size: 40px;
+  font-family: Roboto;
+  white-space: nowrap;
+  letter-spacing: -0.5px;
+`;
+const Googleplay = styled.img`
+  width: 6.88em;
+  height: 110px;
+  margin: 0px 8.69em 0em 0px;
+`;
+const Applelogo = styled.img`
+  width: 6.88em;
+  height: 110px;
+  align-self: flex-start;
+  margin: 0em 0px 0px 0px;
+`;
+const NewGroup1 = styled.div`
+  width: 57.91em;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-self: flex-end;
+  align-items: center;
+  margin: 0px 15.94em 2.91em 0px;
+  padding: 0em 0px;
+`;
+const EasyToEdit = styled.div`
+  margin: 0px 10.75em 0px 0px;
+  color: #060c3a;
+  font-size: 24px;
+  font-family: Roboto;
+  white-space: nowrap;
+  letter-spacing: -0.4px;
+`;
+const Text9 = styled.div`
+  margin: 0px 10.46em 0px 0px;
+  color: #060c3a;
+  font-size: 24px;
+  font-family: Roboto;
+  text-align: center;
+  white-space: nowrap;
+  letter-spacing: -0.4px;
+`;
+const UXUIKit = styled.div`
+  color: #060c3a;
+  font-size: 24px;
+  font-family: Roboto;
+  text-align: center;
+  white-space: nowrap;
+  letter-spacing: -0.4px;
+`;
+const NewGroup2 = styled.div`
+  width: 54.38em;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  margin: 0px 0px 1.78em 0px;
+  padding: 0em 0px;
+`;
+const CeruleanFlexColumn = styled.div`
+  height: 70px;
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0.47em 0.56em 0px 0px;
+  padding: 0px 1.19em 0px 1.25em;
+  border-radius: 35px;
+  background-color: #04befe;
+`;
+const Shape4 = styled.div`
+  height: 18px;
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 0.31em;
+  background-size: cover;
+  background-image: url("https://file.rendit.io/n/42LRUhsvjIGlNpjUTWuj.svg");
+`;
+const Shape5 = styled.img`
+  width: 1.31em;
+  height: 7px;
+`;
+const Lines = styled.div`
+  height: 84px;
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-self: center;
+  align-items: center;
+  margin: 0px 0.63em 0px 0px;
+  padding: 0.47em 20em 0.47em 20.06em;
+  background-size: cover;
+  background-image: url("https://file.rendit.io/n/digttgIRiEzcNj1l0Itd.svg");
+`;
+const MacaroniAndCheeseFlexColumn = styled.div`
+  height: 70px;
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 1.25em;
+  border-radius: 35px;
+  background-color: #ffb88a;
+`;
+const Shape6 = styled.img`
+  width: 1.88em;
+  height: 30px;
+`;
+const IndigoFlexColumn = styled.div`
+  height: 70px;
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0.47em 0px 0px 0px;
+  padding: 0px 1.44em;
+  border-radius: 35px;
+  background-color: #8aabff;
+`;
+const Shape7 = styled.img`
+  width: 1.5em;
+  height: 34px;
+`;
+const NewGroup3 = styled.div`
+  width: 69.38em;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0px 0px 7.06em 0px;
+`;
+const NewGroup4 = styled.div`
+  width: 90em;
+  height: 1920px;
+  position: relative;
+  gap: 99px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  margin: 0px 0px 13.44em 0px;
+`;
+const Bg3 = styled.div`
+  position: relative;
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 6.19em 41.56em 7.56em 4.69em;
+  background-image: linear-gradient(45deg, #629151 0%, #f2f6fd 100%);
+`;
+const Oval = styled.img`
+  width: 43.75em;
+  height: 700px;
+`;
+const Group16 = styled.div`
+  width: 76.88em;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0em 4.69em 0em 8.44em;
+`;
+const Group17 = styled.div`
+  height: 285px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 13.69em 0em 12.63em 0em;
+`;
+const LoremIpsumDolorSi = styled.div`
+  width: 19.38em;
+  align-self: center;
+  margin: 0px 0px 3.44em 0px;
+  color: #8c8e9e;
+  font-size: 16px;
+  font-family: Roboto;
+  line-height: 28px;
+`;
+const Group18 = styled.div`
+  width: 21.88em;
+  height: 706px;
+  position: relative;
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 0em 12.19em 0em 9.69em;
+`;
+const Black = styled.img`
+  width: 21.88em;
+  height: 700px;
+  position: relative;
+`;
+const Qd = styled.div`
+  height: 280px;
+  left: 0px;
+  top: 0px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 32.06em 11.38em 28.19em 58.44em;
+  background-size: cover;
+  background-image: url("https://file.rendit.io/n/2IyVJgdwk9YDOxkTSVZb.png");
+`;
+const LoremIpsumDolorSi1 = styled.div`
+  width: 19.38em;
+  align-self: center;
+  margin: 0px 0px 3.13em 0px;
+  color: #8c8e9e;
+  font-size: 16px;
+  font-family: Roboto;
+  line-height: 28px;
+`;
+const NewGroup5 = styled.div`
+  width: 73.13em;
+  height: 749px;
+  position: relative;
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-self: flex-end;
+  align-items: center;
+  margin: 0px 0em 0px 0px;
+  padding: 0em 8.44em 0em 8.31em;
+`;
+const Bg = styled.div`
+  width: 89.88em;
+  height: 649px;
+  left: 0px;
+  top: 100px;
+  position: absolute;
+  background-image: linear-gradient(45deg, #77a263 0%, #f2f6fd 100%);
+`;
+const Bg1 = styled.div`
+  width: 73.13em;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 5em 0px;
+  border-radius: 30px;
+  background-color: #ffffff;
+  box-shadow: 0px 50px 100px 0px rgba(6, 12, 58, 0.2);
+`;
+const Shape = styled.div`
+  gap: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  margin: 0px 16.25em 0px 0px;
+  padding: 0.44em 0.44em 0.63em 0.63em;
+  background-size: cover;
+  background-image: url("https://file.rendit.io/n/tIkTE5jpHTkkHzHBeBKN.svg");
+`;
+const Shape1 = styled.img`
+  width: 1.44em;
+  height: 23px;
+`;
+const Logofacebook = styled.img`
+  width: 2.5em;
+  height: 40px;
+  margin: 0px 16.13em 0px 0px;
+`;
+const Shape2 = styled.img`
+  width: 2.81em;
+  height: 33.9px;
+  margin: 0px 16.06em 0px 0px;
+`;
+const Shape3 = styled.img`
+  width: 2.5em;
+  height: 40px;
+`;
